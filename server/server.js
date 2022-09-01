6a9632dfc1b028fcbce04c2f40e68d49e42783a9
Al Camara <Alhousseinec@gmail.com>
2022-08-31 21:35:45 -0500

    get get joke from api

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const axios = require('axios');
 const app = express()
+
 
 const post = 5000;
 
@@ -11,5 +13,11 @@ app.listen(post, ()=>{
 })
 
 app.get('/Chuck-jokes', (req,res)=>{
-    console.log('test');
+    axios({
+        url:'https://api.chucknorris.io/jokes/random',
+        method:'GET'
+    }).then((resp)=>{
+        console.log(resp.data.value);
+        res.send(resp.data.value);
+    })
 })
